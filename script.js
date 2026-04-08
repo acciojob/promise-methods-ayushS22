f007d1a490128968d58cf057f7a20bc69ad17d89
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-04-08 10:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
 function myRace(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    promises.forEach((p) => {
+      Promise.resolve(p)
+        .then(resolve)
+        .catch(reject);
+    });
+  });
 }
 
 function myAny(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    let rejectedCount = 0;
+
+    promises.forEach((p) => {
+      Promise.resolve(p)
+        .then(resolve)
+        .catch(() => {
+          rejectedCount++;
+          if (rejectedCount === promises.length) {
+            reject("all promises rejected");
+          }
+        });
+    });
+  });
 }
 
 function myAll(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    let results = [];
+    let completed = 0;
+
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then((value) => {
+          results[index] = value;
+          completed++;
+
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+  });
 }
 
 function myAllSettled(promises) {
   // implementation
+	return new Promise((resolve) => {
+    let results = [];
+    let completed = 0;
+
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then((value) => {
+          results[index] = {
+            status: "fulfilled",
+            value: value,
+          };
+        })
+        .catch((error) => {
+          results[index] = {
+            status: "rejected",
+            error: error,
+          };
+        })
+        .finally(() => {
+          completed++;
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
 }
 
 module.exports = {
